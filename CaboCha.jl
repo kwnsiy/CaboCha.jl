04b5605ac8e89801ed7ce09060ee56abd9d9b6f0
kwnsiy <sei5512kkk@yahoo.co.jp>
2016-12-04 16:48:06 +0900

    Create CaboCha.jl

---
+++ CaboCha.jl
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+using XMLconvert
+
+doc=
+"""
+usage: 
+  cabocha_parser("クロールで泳いでいる少女を見た")
+  This function takes a dictionary as a return value.
+""
+
+""" cabocha_parser """
+fucntion cabocha_parser(sentence)
+  res = readall(pipeline(`echo $sentence`, `cabocha -f3`))
+  return xml2dict(root(parse_string(res)))
+end
+
