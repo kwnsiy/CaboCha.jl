0fc0405f50115147d4250c331a08a689edacd9a3
kwnsiy <sei5512kkk@yahoo.co.jp>
2016-12-06 19:20:20 +0900

    Update example.jl

--- example.jl
+++ example.jl
@@ -3,6 +3,6 @@
 include("CaboCha.jl")
 
 sentence = "クロールで泳いでいる少女を見た"
-xml = cabocha_parser(sentence)
-
-println(xml)
+println(cabocha_parser(sentence, option = "f0"))
+println(cabocha_parser(sentence, option = "f1")) # default
+println(cabocha_parser(sentence, option = "f3"))
